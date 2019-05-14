7d355fc8c1bf1782d5b848304caa85e58e799ac5
Brian <bechatman@gmail.com>
2019-05-13 21:02:41 -0400

    Add markdown filter for templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,30 @@
-module.exports = (eleventyConfig) => {  
+module.exports = (config) => {  
   // Ignore files and pass through
-  eleventyConfig.addPassthroughCopy("./admin");
-  eleventyConfig.addPassthroughCopy('./assets/')
+  config.addPassthroughCopy('./admin');
+  config.addPassthroughCopy('./assets/')
+  
+  let markdownIt = require('markdown-it');
+  let markdownItAnchor = require('markdown-it-anchor');
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let opts = {
+    permalink: false
+  };
+  
+  let md = markdownIt(options)
+    .use(markdownItAnchor, opts)
+  
+  config.setLibrary('md', md);
+  config.addFilter('markdown', value => md.render(value))  
   
   return {
     // Set Layout Directory
     dir: {
-      includes: "_includes",
-      layouts: "_layouts"
+      includes: '_includes',
+      layouts: '_layouts'
     }
   }
 }
